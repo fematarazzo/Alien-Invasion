3d04c73ea596288a2f4389d161b11cbac0a007b5
Felipe Matarazzo <felipemps@protonmail.com>
2022-03-22 17:43:33 -0300

    Movement fix

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,15 +31,15 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT or pygame.KSCAN_D:
+                if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
-                if event.key == pygame.K_LEFT or pygame.KSCAN_A:
+                elif event.key == pygame.K_LEFT:
                     self.ship.moving_left = True
 
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT or pygame.KSCAN_D:
+                if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
-                if event.key == pygame.K_LEFT or pygame.KSCAN_A:
+                elif event.key == pygame.K_LEFT:
                     self.ship.moving_left = False
 
 

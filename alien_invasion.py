f6c696b55049656252d1fb7bd6fa7d4a20ccd5e7
Felipe Matarazzo <felipemps@protonmail.com>
2022-03-22 17:41:36 -0300

    Moving both left and right

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,6 +22,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -29,6 +30,19 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT or pygame.KSCAN_D:
+                    self.ship.moving_right = True
+                if event.key == pygame.K_LEFT or pygame.KSCAN_A:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT or pygame.KSCAN_D:
+                    self.ship.moving_right = False
+                if event.key == pygame.K_LEFT or pygame.KSCAN_A:
+                    self.ship.moving_left = False
+
+
 
     def _update_screen(self):
         """Update images on the screen and flip to the new screen."""

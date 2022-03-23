7ef04008c0e12f2c65a29144b10126cf01a035a8
Felipe Matarazzo <felipemps@protonmail.com>
2022-03-23 11:45:56 -0300

    Moving the aliens

--- settings.py
+++ settings.py
@@ -11,9 +11,13 @@ class Settings:
         # Ship settings
         self.ship_speed = 2.5
 
-        # Bulllet settings
+        # Bullet settings
         self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+
+        # Alien settings
+        self.alien_speed = 1.0
+
